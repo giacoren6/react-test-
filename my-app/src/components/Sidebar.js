d03ba23ee5e22fb47aa237b5cbc81d9b943afe46
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-06-21 18:09:37 +0000

    done

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Sidebar() {
+    return (
+        <div>
+            <h1>Sidebar Component</h1>
+            <div>
+                <a></a>
+                <a></a>
+                <a></a>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
